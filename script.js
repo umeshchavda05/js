9d315232bc9a687cb6f7b5b359484b0d1df4b9e7
Umesh <umeshchavda05@gmail.com>
2022-02-18 13:00:19 +0530

    html commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function display(val){
+    document.getElementById("dis").value+=val
+}
+
+function backspace(){
+    document.getElementById("dis").value = document.getElementById("dis").value.slice(0,-1)
+}
+
+function clr(){
+    document.getElementById("dis").value = ""
+}
+
+function calc(){
+    let x = document.getElementById("dis").value
+    y = eval(x)
+    document.getElementById("dis").value = y 
+}
+
